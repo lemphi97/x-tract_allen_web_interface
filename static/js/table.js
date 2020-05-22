03e18d5f74e80599c2b3e65e0c35d2074746d673
Philippe Lemieux <lemphi97@gmail.com>
2020-05-22 15:37:49 -0400

    js minor cleanup.

--- static/js/table.js
+++ static/js/table.js
@@ -1,5 +1,28 @@
-function parseParenthesesToArray(str) {
+function parseParenthesesToArray(str)
+{
     var noParentheses = str.substring(str.indexOf("(") + 1, str.indexOf(")"));
     var trimWhiteSpace = noParentheses.replace(", ", ",");
     return trimWhiteSpace.split(",");
+}
+
+function validateMinMax(value, min, max)
+{
+    return (isNaN(min) && isNaN(max)) ||
+           (isNaN(min) && value <= max) ||
+           (min <= value && isNaN(max)) ||
+           (min <= value && value <= max);
+}
+
+function validateNames(str, arrayName)
+{
+    // TODO
+    var valid = true;
+    return true;
+}
+
+function validateAcronyms(str, arrayAcron)
+{
+    // TODO
+    var valid = true;
+    return valid;
 }
