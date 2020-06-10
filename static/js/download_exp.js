c6eef3bad13cf485b0aee844704fabf60a97dece
Philippe Lemieux <lemphi97@gmail.com>
2020-06-10 11:06:42 -0400

    download nrrd for each img type.

--- static/js/download_exp.js
+++ static/js/download_exp.js
@@ -6,13 +6,51 @@ exp_id = 0;
 function download_grid_data(id, img, res)
 {
     var url = "http://api.brain-map.org/grid_data/download_file/" + id + "?image=" + img + "&resolution=" + res;
-    //alert(url);
     let a = document.createElement('a')
     a.href = url
-    //a.download = url.split('/').pop()
     document.body.appendChild(a)
     a.click()
     document.body.removeChild(a)
+}
+
+/*
+available :
+    - projection_energy
+    - projection_density
+    - injection_fraction
+    - injection_density
+    - injection_energy
+    - data_mask
+*/
+
+function download_projection_density()
+{
+    download_grid_data(exp_id, "projection_density", res_nrrd)
+}
+
+function download_projection_energy()
+{
+    download_grid_data(exp_id, "projection_energy", res_nrrd)
+}
+
+function download_injection_fraction()
+{
+    download_grid_data(exp_id, "injection_fraction", res_nrrd)
+}
+
+function download_injection_density()
+{
+    download_grid_data(exp_id, "injection_density", res_nrrd)
+}
+
+function download_injection_energy()
+{
+    download_grid_data(exp_id, "injection_energy", res_nrrd)
+}
+
+function download_data_mask()
+{
+    download_grid_data(exp_id, "data_mask", res_nrrd)
 }
 
 $(document).ready(function ()
