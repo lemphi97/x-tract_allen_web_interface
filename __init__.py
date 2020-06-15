6dfd2bc1fb9eda326cd5b800ac32ade73af89bf4
Philippe Lemieux <lemphi97@gmail.com>
2020-06-15 00:04:56 -0400

    cleaner rendering for templates.

--- __init__.py
+++ __init__.py
@@ -24,20 +24,46 @@ socketio = SocketIO(app)
 Compress(app)
 
 # Global variables
-home_require_update = True
-interface_require_update = True
-exp_require_update = True
-volume_require_update = True
-about_web_require_update = True
 all_exp = utils.get_all_exp()
 st_dict = utils.get_struct_in_dict(all_exp)
+
+@app.before_first_request
+def render_templates():
+    # home
+    rendered_template = flask.render_template("index.html.j2")
+    with open(app.static_folder + "/html/rendered_template/index.html", "w") as f:
+        f.write(rendered_template)
+
+    # interface
+    rendered_template = flask.render_template("interface.html.j2")
+    with open(app.static_folder + "/html/rendered_template/interface.html", "w") as f:
+        f.write(rendered_template)
+
+    # allen_brain
+    rendered_template = flask.render_template(
+        "allen_brain.html.j2",
+        all_exp=all_exp,
+        struct_dict=st_dict
+    )
+    with open(app.static_folder + "/html/rendered_template/allen_brain.html", "w") as f:
+        f.write(rendered_template)
+
+    # volume_viewer
+    rendered_template = flask.render_template("volume_viewer.html.j2")
+    with open(app.static_folder + "/html/rendered_template/volume_viewer.html", "w") as f:
+        f.write(rendered_template)
+
+    # about_website
+    rendered_template = flask.render_template("about_website.html.j2")
+    with open(app.static_folder + "/html/rendered_template/about_website.html", "w") as f:
+        f.write(rendered_template)
 
 @socketio.on('req_download', namespace="/request_zip")
 def handle_download_request(req_id, req_img, res):
     download_id = flask.request.sid
     tmp_folder = app.static_folder + "/tmp/" + download_id
 
-    # download experiments nrrd
+    # download experiments nrrd #todo wayyy to slow
     for exp_id in req_id:
         utils.exp_save_nrrd(exp_id, req_img, res, tmp_folder)
 
@@ -66,6 +92,7 @@ def request_zip(zip_id):
         attachment_filename='data.zip'
     )
     #https://stackoverflow.com/questions/24612366/delete-an-uploaded-file-after-downloading-it-from-flask
+    #https://stackoverflow.com/questions/9419162/download-returned-zip-file-from-url
 
 @app.route("/")
 def default():
@@ -73,45 +100,16 @@ def default():
 
 @app.route("/home/")
 def home():
-    global home_require_update
-    if home_require_update:
-        rendered_template = flask.render_template("index.html.j2")
-        with open(app.static_folder + "/html/rendered_template/index.html", "w") as f:
-            f.write(rendered_template)
-        home_require_update = False
-        return rendered_template
-    else:
-        # https://stackoverflow.com/questions/24578330/flask-how-to-serve-static-html
-        return flask.current_app.send_static_file('html/rendered_template/index.html')
+    # https://stackoverflow.com/questions/24578330/flask-how-to-serve-static-html
+    return flask.current_app.send_static_file('html/rendered_template/index.html')
 
 @app.route("/interface/")
 def interface():
-    global interface_require_update
-    if interface_require_update:
-        rendered_template = flask.render_template("interface.html.j2")
-        with open(app.static_folder + "/html/rendered_template/interface.html", "w") as f:
-            f.write(rendered_template)
-        interface_require_update = False
-        return rendered_template
-    else:
-        return flask.current_app.send_static_file('html/rendered_template/interface.html')
-    return flask.render_template("interface.html")
+    return flask.current_app.send_static_file('html/rendered_template/interface.html')
 
 @app.route("/experiments/")
 def experiments():
-    global exp_require_update
-    if exp_require_update:
-        rendered_template = flask.render_template(
-            "allen_brain.html.j2",
-            all_exp=all_exp,
-            struct_dict=st_dict
-        )
-        with open(app.static_folder + "/html/rendered_template/allen_brain.html", "w") as f:
-            f.write(rendered_template)
-        exp_require_update = False
-        return rendered_template
-    else:
-        return flask.current_app.send_static_file('html/rendered_template/allen_brain.html')
+    return flask.current_app.send_static_file('html/rendered_template/allen_brain.html')
 
 @app.route("/experiments/<exp_id>/")
 def experiment(exp_id):
@@ -123,27 +121,11 @@ def experiment(exp_id):
 
 @app.route("/volume_viewer/")
 def volume_viewer():
-    global volume_require_update
-    if volume_require_update:
-        rendered_template = flask.render_template("volume_viewer.html.j2")
-        with open(app.static_folder + "/html/rendered_template/volume_viewer.html", "w") as f:
-            f.write(rendered_template)
-        volume_require_update = False
-        return rendered_template
-    else:
-        return flask.current_app.send_static_file('html/rendered_template/volume_viewer.html')
+    return flask.current_app.send_static_file('html/rendered_template/volume_viewer.html')
 
 @app.route("/about_website/")
 def about_website():
-    global about_web_require_update
-    if about_web_require_update:
-        rendered_template = flask.render_template("about_website.html.j2")
-        with open(app.static_folder + "/html/rendered_template/about_website.html", "w") as f:
-            f.write(rendered_template)
-        about_web_require_update = False
-        return rendered_template
-    else:
-        return flask.current_app.send_static_file('html/rendered_template/about_website.html')
+    return flask.current_app.send_static_file('html/rendered_template/about_website.html')
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
